fd45b51363dd53211967d7e38cbee6bc94d253b2
Othniel Powell <othniel@live.ca>
2025-11-28 19:30:38 -0500

    changed app.js and check status of web site

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import ProgressScreen from "./screens/ProgressScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <ProgressScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
